820228215b3970302ff7bb5a7d044ce00af49628
Zack Barbari <zackb@unc.edu>
2022-02-24 17:56:38 -0500

    Added wrong call for guess flip

--- guess-flip.js
+++ guess-flip.js
@@ -2,7 +2,7 @@ import {flipACoin} from "./modules/coin.mjs"
 import minimist from "minimist"
 
 const args = minimist(process.argv.slice(2))
-if (args.call == null) {
+if (args.call == null || args.call != "heads" || args.call != "tails") {
     console.log("Error: no input.")
     console.log("Usage: node guess-flip --call=[heads|tails]")
 } else {
